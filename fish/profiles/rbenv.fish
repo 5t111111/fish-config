26ab5d8c07eebbaf0173b368d57582bb8e78fcc4
Hirofumi Wakasugi <baenej@gmail.com>
2017-04-05 10:52:53 +0900

    Fix yarn profile path

--- fish/profiles/rbenv.fish
+++ fish/profiles/rbenv.fish
@@ -3,8 +3,8 @@ if type rbenv >/dev/null ^/dev/null
         set -l rbenv_path "$HOME/.rbenv"
 
         if test -d "$rbenv_path"
-            set PATH "$rbenv_path/bin" $PATH
-            set PATH "$rbenv_path/shims" $PATH
+            # set -x PATH "$rbenv_path/bin" $PATH
+            set -x PATH "$rbenv_path/shims" $PATH
             rbenv rehash >/dev/null ^/dev/null
         end
     end
--- fish/profiles/yarn.fish
+++ fish/profiles/yarn.fish
@@ -1,3 +1,3 @@
 if type yarn >/dev/null ^/dev/null
-    set -x $PATH "$PATH:(yarn global bin)"
+    set -x PATH $PATH (yarn global bin)
 end
