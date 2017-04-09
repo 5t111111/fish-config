10d1716a64ab7324d77b25d3796541fb9ca4afc9
Hirofumi Wakasugi <baenej@gmail.com>
2017-04-10 00:36:15 +0900

    Update rbenv configuration

--- fish/profiles/rbenv.fish
+++ fish/profiles/rbenv.fish
@@ -1,11 +1,10 @@
-if type rbenv >/dev/null ^/dev/null
-    begin
-        set -l rbenv_path "$HOME/.rbenv"
+begin
+    set -l rbenv_path "$HOME/.rbenv"
 
-        if test -d "$rbenv_path"
-            # set -g fish_user_paths "$rbenv_path/bin" $fish_user_paths
-            set -g fish_user_paths "$rbenv_path/shims" $fish_user_paths
-            rbenv rehash >/dev/null ^/dev/null
-        end
+    if test -d "$rbenv_path/bin"
+    	set -g fish_user_paths "$rbenv_path/bin" $fish_user_paths
     end
+
+    set -g fish_user_paths "$rbenv_path/shims" $fish_user_paths
+    rbenv rehash >/dev/null ^/dev/null
 end
