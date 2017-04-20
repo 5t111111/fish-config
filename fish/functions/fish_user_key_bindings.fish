ebce2795574c5a2ee2f71afb3f7a7aedf2cf784f
Hirofumi Wakasugi <baenej@gmail.com>
2017-04-20 10:43:14 +0900

    Add function to select filename from git status via peco

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -3,5 +3,6 @@ function fish_user_key_bindings
     bind \cg peco_select_repository
     bind \ef peco_find
     bind \eo peco_git_checkout
+    bind \es peco_git_status
     bind \ek peco_kill_process
 end
