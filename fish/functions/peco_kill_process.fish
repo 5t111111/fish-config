a5c1de31bdbb25bec110d2465366306a4e333347
Hirofumi Wakasugi <baenej@gmail.com>
2017-04-22 10:26:07 +0900

    Extract functions to plugins part2

--- fish/functions/peco_kill_process.fish
+++
@@ -1,8 +0,0 @@
-function peco_kill_process -d "Peco source to select and kill process"
-    ps aux | peco "PROCESS>" | awk '{ print $2 }' | read pid
-
-    if test -n "$pid"
-        commandline "kill $pid"
-        commandline -f execute
-    end
-end
