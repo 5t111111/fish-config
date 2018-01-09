692ec006debbd02048a118912bb76cfcaf380fd1
Hirofumi Wakasugi <baenej@gmail.com>
2018-01-09 09:38:07 +0900

    Add path to Go typical Linux installation bin

--- fish/config.fish
+++ fish/config.fish
@@ -30,6 +30,11 @@ if not contains "$HOME/.local/bin" $fish_user_paths
     set -g fish_user_paths "$HOME/.local/bin" $fish_user_paths
 end
 
+# Go typical Linux installation bin
+if not contains "/usr/local/go/bin" $fish_user_paths
+    set -g fish_user_paths "/usr/local/go/bin" $fish_user_paths
+end
+
 # Initalize pluggable environent settings
 go_init
 rbenv_init
